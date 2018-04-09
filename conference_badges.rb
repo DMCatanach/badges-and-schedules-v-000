fcbfd4d87c4b876b83c0934734f6384f3f48133c
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-09 00:22:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,10 @@
 # Write your code here.
-def badge_maker(name) 
-  "Hello, my name is #{name}." 
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(speakers) 
+  speakers.collect do |speaker| 
+    "Hello, my name is #{speaker}." 
+  end 
 end 
